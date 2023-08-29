c7b89b043a4eefff7c0880ef67a69138bd8d285d
AMandTM20 <131997423+AMandTM20@users.noreply.github.com>
2023-08-29 11:05:02 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
-
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
-#hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    image = models.URLField(max_length=100)
+    release_date = models.DateField( auto_now=False, auto_now_add=False)
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
