cd49f49004f5209e6969faa818100fd586259a9b
AMandTM20 <131997423+AMandTM20@users.noreply.github.com>
2023-08-29 11:05:42 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
-"""recipes URL Configuration
+"""main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.urls import path
 
+
+
+from django.contrib import admin
+from django.urls import path
+import phones.views
+
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('admin/', admin.site.urls),
+    path('', phones.views.index),
+    path('catalog/', phones.views.show_catalog, name='catalog'),
+    path('catalog/<slug:slug>/', phones.views.show_product, name='phone'),
 ]
+
