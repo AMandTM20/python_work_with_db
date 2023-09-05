5f7b29e976a0706ecd3b9aff4837ad8435a4f0d2
AMandTM20 <131997423+AMandTM20@users.noreply.github.com>
2023-09-05 13:14:31 +0300

    Add files via upload

--- import_phones.py
+++ import_phones.py
@@ -3,26 +3,30 @@ import csv
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
+
 class Command(BaseCommand):
+    help = 'Импортировать csv файл'
     def add_arguments(self, parser):
-        #parser.add_argument('phone_ids', nargs='+', type=int) # Не надо ли было так дописать эту функцию? Потом использовать  переменную phone_ids в ф-ции def handle?
         pass
 
-    def handle(self, *args, **options): # почему здесь options, а не kwargs?
+    def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-             # TODO: Добавьте сохранение модели # что тут имеется ввиду?- выполнение Phone.objects.create?
-            # оператор Phone.objects.create( создает и записывает в базу данных новый объект-строку(?)(cохраняет в базе данных)
-            Phone.objects.create( # Почему-то везде objects подсвечивается желтым цветом
-                # Дальше идет заполнение строки таблицы в базе данных?
-            id = phone['id'],
-            name = phone['name'],
-            price = phone['price'],
-            image = phone['image'],
-            release_date = phone['release_date'],
-            lte_exists = phone['lte_exists']
+            p = Phone.objects.create(
+                        id=phone['id'],
+                        name=phone['name'],
+                        price=phone['price'],
+                        image=phone['image'],
+                        release_date=phone['release_date'],
+                        lte_exists=phone['lte_exists'],
+                        #slug=phone['slug'] # Пытался сделать так, но выдавалась ошибка KeyError: 'slug'
+                        # присвоение значения переменной slug происходит в файле models.py при вызове функции save, переопределенной в models.py:
+                        #self.slug = slugify(self.name). Slug применяется к имени телефона, делая имя более удобным  для набора  его в адресной строке
+
+
             )
-        # Здесь уже не надо сохранять строку в таблице базы данных(так как Phone.objects.create ее сохраняет?)
-        # Может быть, здесь надо сохранить всю таблицу?
+            p.save()# вызов фукции save, переопределенной в файле models.py
+
+
