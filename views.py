3e3f4fcf07278b1c2842fa11ede764e6f02ec7d9
AMandTM20 <131997423+AMandTM20@users.noreply.github.com>
2023-09-05 13:17:24 +0300

    Add files via upload

--- views.py
+++ views.py
@@ -1,35 +1,24 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
-from phones.models import Phone
 
 def index(request):
-    # получение данных из моделей
-    all_phones = Phone.objects.all()# получить все объекты из нашей модели(все записи нашей таблицы), objects - менеджер(?)
-    print('all_phones :',all_phones)
-
     return redirect('catalog')
 
 
 def show_catalog(request):
     template = 'catalog.html'
-    phones_all = Phone.objects.all()
-    name = request.GET.get('name')
-    price = request.GET.get('price')
-    context = {'phones': phones_all,
-                'name': name,
-                'price': price }
+    # Передается словарь, где значения  переменных - это выбранные из адресной строки параметры,
+    # отсортированные по имени, или по возрастанию цены, или по убыванию цены
+    # Мы таких сложных примеров не изучали!!!
+    context = {"phones": Phone.objects.all().order_by
+    ({'name': 'name', 'min_price': 'price', 'max_price': '-price', None: 'id'}[request.GET.get('sort')])}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    phones = Phone.objects.filter(slug = slug)# ?
-    context = {'phones': phones }
+    # Передается словарь, содержащий один элемент. Значение переменной - отслагированное имя телефона,
+    # набранное пользователем в адресной строке(динамический параметр)
+    context = {"phone": Phone.objects.get(slug=request.get_full_path().split('/')[-2])}
     return render(request, template, context)
-
-
-
-
-
-
-
