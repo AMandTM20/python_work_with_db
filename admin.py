26014562c7ee161bb05678f97630023c0a1b0094
AMandTM20 <131997423+AMandTM20@users.noreply.github.com>
2023-09-05 13:14:05 +0300

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,15 +1,8 @@
-
 from django.contrib import admin
 from .models import Phone
 
-# # Register your models here.
-@admin.register(Phone)
-class PhoneAdmin (admin.ModelAdmin):
-    list_display = ['id', 'name', 'price', 'image', 'release_date',
-    'lte_exists', 'slug']
-    list_filter = ['name', 'price']
-    prepopulated_fields = {'slug': ('name',)}
+@admin.register(Phone)# декоратор @admin регистрирует класс PhoneAdmin в качестве администратора для модели Phone
+class PhoneAdmin(admin.ModelAdmin):
+    # вывод на экран данных о телефонах (при вызове cтраницы сайта  admin)
+    list_display = ['id', 'name', 'image', 'price', 'release_date', 'lte_exists', 'slug']
 
-# @admin.register(Phone)
-# class PhoneAdmin(admin.ModelAdmin):
-#     pass
